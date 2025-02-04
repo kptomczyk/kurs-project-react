79aeb7c919d8ec08fd5c27280327a0cf1ecf8c2f
Krystian Tomczyk <kontakt@kptomczyk.pl>
2025-02-04 19:22:54 +0100

    rebuild notifications

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Layout } from "../components/Layout";
+
+export const Home = () => (
+    <Layout>
+        <div className="main-content right-chat-active">
+            <div className="middle-sidebar-bottom">
+                <div className="middle-sidebar-left">
+                    <div className="row feed-body"></div>
+                </div>
+            </div>
+        </div>
+    </Layout>
+);
